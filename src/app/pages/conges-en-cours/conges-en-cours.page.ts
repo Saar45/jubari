929af0099860cbe5dc3d11b43e96642cede914e0
Saar45 <nabysarr16@gmail.com>
2025-04-01 21:22:00 +0200

    Add CongesEnCours page with loading and error handling for current leaves

---
+++ src/app/pages/conges-en-cours/conges-en-cours.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { VibraniumService } from '../../services/vibranium.service';
+import { Conge } from '../../models/conge.model';
+
+@Component({
+  selector: 'app-conges-en-cours',
+  templateUrl: './conges-en-cours.page.html',
+  styleUrls: ['./conges-en-cours.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule]
+})
+export class CongesEnCoursPage implements OnInit {
+  conges: Conge[] = [];
+  isLoading = true;
+  error: string | null = null;
+
+  constructor(private vibraniumService: VibraniumService) {}
+
+  ngOnInit() {
+    this.loadConges();
+  }
+
+  private loadConges() {
+    this.isLoading = true;
+    this.vibraniumService.getCurrentConges().subscribe({
+      next: (conges) => {
+        this.conges = conges;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading current leaves:', error);
+        this.error = 'Impossible de charger les congés en cours';
+        this.isLoading = false;
+      }
+    });
+  }
+
+  doRefresh(event: any) {
+    this.loadConges();
+    event.target.complete();
+  }
+}
