a72bea1076fd6ded1ea31d7c9d38a182dc352775
Saar45 <nabysarr16@gmail.com>
2025-04-01 21:22:30 +0200

    Refactor main.ts to use bootstrapApplication for Angular and Ionic integration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicRouteStrategy } from '@ionic/angular';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideIonicAngular(),
+    provideRouter(routes),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    JwtHelperService,
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    provideHttpClient(withInterceptorsFromDi())
+  ]
+});
